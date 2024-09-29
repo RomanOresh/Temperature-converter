825ab22fc2fe38b0d9eb4d09892c08f9e35c3195
Roman <romanoreshchenko@gmail.com>
2024-09-29 12:46:29 +0200

    Added function to convert Celsius to Fahrenheit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,19 @@ import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Приложение для конвертации температуры");
-        int fahrenheit = 49;
+        int fahrenheit = 37;
         double calculatedCelsius = ConvertFrtToCls(fahrenheit);
         DecimalFormat myFormat = new DecimalFormat("#.##");
         System.out.println(fahrenheit + " градусов по фаренгейту, будет " + myFormat.format(calculatedCelsius) + " градусов по цельсию");
+        int celsius = 12;
+        double calculatedFahrenheit = ConvertClsToFrt(celsius);
+        System.out.println(celsius + " градусов по цельсию, будет " + calculatedFahrenheit + " градусов по фаренгейту");
     }
 
     public static double ConvertFrtToCls(int frt){
         return (double) (frt - 32) * 5/9;
+    }
+    public static double ConvertClsToFrt(int cls){
+        return cls * 1.8 + 32;
     }
 }
